9865449418c02afa4be6931e40c9232e6cebf67d
gdwnsam <godwinsam.d@gmail.com>
2022-04-21 15:21:01 +0530

    updated with scroll to index basic steps

--- index.js
+++ index.js
@@ -1,56 +1,42 @@
-// const factorial = (input) => {
-//   let num = 1;
-//   for (let i = input; i >= 1; --i) {
-//     console.log(i);
-//     num *= i;
-//   }
 
-//   return num;
-// };
+// Import stylesheets
+import './style.css';
 
-// // console.log(factorial(2));
+console.log('test')
+  console.log('reportWindowSize triggered')
+  console.log(document.querySelector('.container'))
+  const containerDiv = document.querySelector('.container');
+  const containerDivItem = document.querySelector('.container > div');
 
-// const test = "abcd";
 
-// console.log(test);
-// console.log(test.length);
-// console.log(test[0]);
+function moveToIndex() {
+  console.log(containerDiv.clientHeight);
+  console.log(containerDivItem.clientHeight);
+  containerDiv.scrollTo({top: 20*containerDivItem.clientHeight, behavior: 'smooth'});
+}
 
-var pivotIndex = function (nums) {
-  if (nums.length === 0) return -1;
-  if (nums.length === 1) return 0;
 
-  const totalSum = nums.reduce((acc, cur) => acc + cur);
-  let leftSum = 0;
+window.addEventListener('resize', reportWindowSize);
+containerDiv.addEventListener('mousedown', initDrag, false);
 
-  let test = 0;
-  for (j = 0; j < nums.length - 1; j++) {
-    test += nums[j];
-  }
 
-  console.log("test :::", test);
+moveToIndex()
+function reportWindowSize() {
+  console.log('reportWindowSize')
+  moveToIndex()
+}
 
-  for (i = 0; i < nums.length; i++) {
-    console.log("totalSum :::", totalSum);
-    console.log("leftSum :::", leftSum);
-    console.log("nums[i] :::", nums[i]);
-    console.log(
-      "totalSum - leftSum - nums[i] :::",
-      totalSum - leftSum - nums[i]
-    );
-    if (leftSum === totalSum - leftSum - nums[i]) {
-      return i;
-    }
-    leftSum += nums[i];
-  }
+function reportElementSize() {
+  console.log('reportElementSize')
+  moveToIndex()
+}
 
-  return -1;
-};
+function initDrag(e) {
+  document.documentElement.addEventListener('mousemove', reportElementSize, false);
+  document.documentElement.addEventListener('mouseup', stopDrag, false);
+}
 
-console.log(pivotIndex([1, 2, 3, 2, 1]));
-
-// P[P.length - 1]
-
-// P[5 - 1] = P[4]
-
-// P[i] = nums[0] + nums[1] + ... + nums[i-1]
+function stopDrag(e) {
+  document.documentElement.removeEventListener('mousemove', reportElementSize, false);
+  document.documentElement.removeEventListener('mouseup', stopDrag, false);
+}
