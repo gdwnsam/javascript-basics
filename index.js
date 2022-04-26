9a4e178c5ee305d5f1cee6a37a15222472a54dec
gdwnsam <godwinsam.d@gmail.com>
2022-04-26 09:14:48 +0530

    for mouse move up and down

--- index.js
+++ index.js
@@ -2,32 +2,22 @@
 // Import stylesheets
 import './style.css';
 
-console.log('test')
-  console.log('reportWindowSize triggered')
-  console.log(document.querySelector('.container'))
-  const containerDiv = document.querySelector('.container');
-  const containerDivItem = document.querySelector('.container > div');
-
+const containerDiv = document.querySelector('.container');
+const containerDivItem = document.querySelector('.container > div');
 
 function moveToIndex() {
-  console.log(containerDiv.clientHeight);
-  console.log(containerDivItem.clientHeight);
   containerDiv.scrollTo({top: 20*containerDivItem.clientHeight, behavior: 'smooth'});
 }
-
 
 window.addEventListener('resize', reportWindowSize);
 containerDiv.addEventListener('mousedown', initDrag, false);
 
-
 moveToIndex()
 function reportWindowSize() {
-  console.log('reportWindowSize')
   moveToIndex()
 }
 
 function reportElementSize() {
-  console.log('reportElementSize')
   moveToIndex()
 }
 
